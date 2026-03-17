2641d0d69a5148a506306379fb1052af870dee5b
Brandon Kelly <kbrandon863@gmail.com>
2026-03-16 17:21:24 -0700

    feat(auth-ui) add session username nav display and polished login/sign up forms

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,6 +12,7 @@ router.post("/", async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.username = userData.name;
       req.session.logged_in = true;
       res.status(200).json(userData);
     });
@@ -50,6 +51,7 @@ router.post("/login", async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      req.session.username = userData.name;
 
       res.status(200).json({ User: userData, message: "Login successful" });
     });
