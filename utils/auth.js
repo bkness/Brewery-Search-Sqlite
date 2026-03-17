839a61bd06179b457e7ed21c921aa2c683a736ed
Brandon Kelly <kbrandon863@gmail.com>
2026-03-17 14:29:07 -0700

    feat: enhance UI/UX, modal map interactions, and auth handling

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,11 @@
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
   if (!req.session.logged_in) {
-    res.redirect('/login');
+    if (req.originalUrl.startsWith('/api/')) {
+      return res.status(401).json({ message: 'Please log in first' });
+    }
+
+    return res.redirect('/login');
   } else {
     next();
   }
